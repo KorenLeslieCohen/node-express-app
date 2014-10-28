39ab921aa5dfb24b915d55120b2e62e667055b3b
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-10-28 18:55:11 -0400

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'); // requires Express functionality
+var router = express.Router(); // attaches "router" variable to router method
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET newroute page. */
+router.get('/newroute', function(req, res) { 
+  res.render('newroute', { title: 'New Route' });
 });
 
 module.exports = router;
