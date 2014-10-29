6e66cd251f8dfa18e3ab87c499e3788828b59cff
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-10-28 22:17:51 -0400

    passed data to views

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,23 @@ var router = express.Router(); // attaches "router" variable to router method
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express',
+    author: 'Koren' 
+  });
 });
 
 /* GET newroute page. */
-router.get('/newroute', function(req, res) { 
-  res.render('newroute', { title: 'New Route' });
+router.get('/newroute', function(req, res) {
+  var animals = [
+    {name: "Porky", type: "pig"},
+    {name: "Sugar", type: "dog"},
+    {name: "Mr. Bigglesworth", type: "cat"}
+  ]; 
+  res.render('newroute', { 
+    title: 'New Route',
+    animals: animals
+  });
 });
 
 module.exports = router;
