abc33060a10ad81d8b7ae6a3f624aa8c9f589f1b
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-10-28 20:17:52 -0400

    change to app.js

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('layout', 'layout');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
